c7f56f67eb1b1085e92a68f5e6f94fcf73e60412
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-04-22 23:58:57 +0530

    fix: trail 6

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-import createMDX from '@next/mdx';
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  experimental: {
-    inlineCss: true,
-    useCache: false,
-  },
+  /* config options here */
 };
 
-const withMDX = createMDX();
-
-export default withMDX(nextConfig);
+export default nextConfig;
