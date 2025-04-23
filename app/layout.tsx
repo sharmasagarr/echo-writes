fcb5a5cc2e1819007b252b65960ad52e128cebd8
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-04-23 16:13:36 +0530

    fix: trail 2 mobile responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
       className={`${mynerve.className} ${niconne.className} ${outfit.variable}`}
       suppressHydrationWarning
     >
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>
         <ThemeProvider
           attribute="class" 
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,25 +5,42 @@ import Image from "next/image"
 import SearchBar from "@/components/SearchBar";
 import UserProfile from "@/components/UserProfile";
 import ThemeToggle from "@/components/ThemeToggle";
-import { useState } from "react";
+import { useState, useEffect, useRef } from 'react';
 
 const NavBar = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    
+
+    useEffect(() => {
+        const handler = (event: MouseEvent) => {
+          if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+            setIsOpen(false);
+          }
+        };
+        if (isOpen) {
+          document.addEventListener('mousedown', handler);
+        }
+        return () => {
+          document.removeEventListener('mousedown', handler);
+        };
+    }, [isOpen]);
 
     return (
-        <header className="flex items-center max-w-full dark:bg-gray-800 bg-[#0066ff] text-white dark:text-black shadow-xl">
-            <nav className="flex justify-between items-center w-full">
+        <header className="flex items-center max-w-full h-[3rem] lg:h-[5rem] dark:bg-gray-800 bg-[#0066ff] text-white dark:text-black shadow-xl">
+            <nav className="flex justify-between items-center w-full mx-[1.5rem] lg:mx-[5rem]">
                 <div className="logo flex justify-center items-center gap-3">
                 <Image
                     src="/logo-white.svg"
                     alt="logo-white"
                     width={72}
                     height={72}
-                    className="sm:w-[72px] w-[48px]" // Smaller logo only on mobile
+                    className="w-[40px] lg:w-[72px] "
                 />
-                <span className="mynerve">echoWrites</span>
+                <span className="mynerve text-xl lg:text-3xl ">echoWrites</span>
                 </div>
-                <div className="hidden md:flex justify-between items-center gap-6 text-white text-lg">
+                <div className="hidden lg:flex justify-between items-center gap-6 text-white text-lg">
                     <Link href="/">Home</Link>
                     <Link href="/">Explore</Link>
                     <Link href="/">Write</Link>
@@ -33,13 +50,29 @@ const NavBar = () => {
                     <ThemeToggle />
                 </div>
 
-                <div className="md:hidden ml-auto">
+                <div className="lg:hidden">
                     <button
-                        className="text-2xl"
-                        onClick={() => setIsOpen(!isOpen)}
+                        className="text-xl"
+                        onClick={() => setIsOpen((prev) => !prev)}
                     >
                         ☰
                     </button>
+
+                    <div
+                        ref={menuRef}
+                        className={`absolute top-13 left-0 w-full bg-[#0066ff] px-4 py-3 shadow-md flex-col space-y-3 ${
+                            isOpen ? 'flex opacity-100' : 'opacity-0 pointer-events-none'
+                        } lg:hidden z-10`}
+                        
+                    >
+                    <Link href="/">Home</Link>
+                    <Link href="/">Explore</Link>
+                    <Link href="/">Write</Link>
+                    <Link href="/">About</Link>
+                    <SearchBar />
+                    <UserProfile />
+                    <ThemeToggle />
+                    </div>
                 </div>
             </nav>
         </header>
