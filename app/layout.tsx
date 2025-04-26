9d6e6a3f4f044dafde419054d156bc1cf21257aa
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-04-27 01:23:09 +0530

    fix: changed hero main line

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { Providers } from "./providers";
-import { Mynerve, Niconne, Outfit } from 'next/font/google'
+import { Mynerve, Inter, Outfit } from 'next/font/google'
 
 const mynerve = Mynerve({ weight: '400', subsets: ['latin'] })
-const niconne = Niconne({ weight: '400', subsets: ['latin'] })
+const inter = Inter({ weight: '400', subsets: ['latin'] })
 const outfit = Outfit({ subsets: ['latin'], variable: '--font-outfit' })
 
 export const metadata: Metadata = {
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" 
-      className={`${mynerve.className} ${niconne.className} ${outfit.variable}`}
+      className={`${mynerve.className} ${inter.className} ${outfit.variable}`}
       suppressHydrationWarning
     >
       <head>
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,9 +7,9 @@ const HeroSection = () => {
   return (
     <div className="p-6 lg:p-10 flex justify-center items-center dark:bg-gray-700 dark:bg-none bg-gradient-to-r from-[#c1daff] via-[#bfd9ff] to-[#8fbcff]">
         <div className="p-2 lg:p-5 w-200 h-auto flex flex-col items-center gap-5">
-            <Image src={LogoBlue} alt="logo-blue" width={100} height={100} className="block dark:hidden lg:w-[150px] lg:h-[150px]" />
+            <Image src={LogoBlue} alt="logo-blue" width={100} height={100} className="shadow-blue-900 block dark:hidden lg:w-[150px] lg:h-[150px]" />
             <Image src={LogoWhite} alt="logo-blue" width={100} height={100} className="hidden dark:block lg:w-[150px] lg:h-[150px]"/>
-            <div className="niconne text-2xl lg:text-4xl text-center">Resonate, Write, Inspire</div>
+            <div className="font-medium text-2xl lg:text-4xl text-center">Where <span className="text-orange-500">thoughts</span> become stories,<br /><span className="text-orange-500">Stories</span> that inspire minds</div>
             <div className="text-xs lg:text-sm text-center text-gray-500 dark:text-gray-300">Your ultimate platform for creating, editing, and sharing engaging blogs. Empower your voice, connect with readers, and turn your ideas into captivating stories. Start your blogging journey today!</div>
             <div className="flex justify-center gap-2">
                 <Button className="bg-[#0066ff] hover:bg-[#7266cb] dark:bg-white dark:text-black dark:hover:bg-white">Get Started</Button>
