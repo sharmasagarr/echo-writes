803ec50e778fc8673f2fc9814d7cd16d3639a922
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-05-06 23:08:20 +0530

    fix: fixed temporary author image

--- components/AddComment.tsx
+++ components/AddComment.tsx
@@ -20,7 +20,7 @@ export default function AddComment({postId, setComments} : {postId: string, setC
       _createdAt: new Date().toISOString(),
       author: {
         name: session?.user?.name ?? "Anonymous",
-        image: session?.user?.image ?? undefined,
+        image: undefined,
       },
     };
   
