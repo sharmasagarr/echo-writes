66025811cc70139b9577da9596fd028d0fd6e1a4
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-05-18 13:08:19 +0530

    fix: change enviroment variable name for auth secret

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,11 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET,
+  });
+
   console.log("Token:", token);
 
   const isAuth = !!token;
