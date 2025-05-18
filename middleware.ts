0ed7c2bc21375d26c0afd35bddbad547bf35dfb3
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-05-18 12:39:59 +0530

    debug: log the token value to see in production

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+  console.log("Token:", token);
 
   const isAuth = !!token;
 
