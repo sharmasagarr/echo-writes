a5ce0ece0198c91dd5289804fc788a804a51b855
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-05-18 15:15:05 +0530

    fix: do code cleaning after final fix

--- middleware.ts
+++ middleware.ts
@@ -7,10 +7,8 @@ export async function middleware(request: NextRequest) {
     req: request,
     secret: process.env.AUTH_SECRET,
     cookieName: "__Secure-authjs.session-token",
-    secureCookie: process.env.NODE_ENV === "production",
   });
 
-  console.log("Token in middleware:", token);
   const isAuth = !!token;
   const path = request.nextUrl.pathname;
   const protectedPaths = ["/profile"];
